d34b7861a1475929f87417ac1c96da36e01eb067
Polka37 <209836559+Polka37@users.noreply.github.com>
2025-11-22 16:49:21 +0100

    Change coke from carbon icon

--- prototypes/recipes/smelting.lua
+++ prototypes/recipes/smelting.lua
@@ -17,6 +17,14 @@ data:extend({
 	{
 		type = "recipe",
 		name = "kr-coke-carbon",
+		icons = {
+			{ icon = "__Krastorio2Assets__/icons/items/coke.png" },
+			{
+				icon = "__space-age__/graphics/icons/carbon.png",
+				scale = 0.33,
+				shift = { -8, -8 },
+			},
+		},
 		enabled = false,
 		category = "smelting",
 		energy_required = 16,
