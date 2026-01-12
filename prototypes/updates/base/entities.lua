a78e1fab8d78389c0b94a88e412c1d6b4b41d6c3
Polka37 <209836559+Polka37@users.noreply.github.com>
2026-01-12 23:32:14 +0100

    remove kr-explosion dmg type

--- prototypes/updates/base/entities.lua
+++ prototypes/updates/base/entities.lua
@@ -440,7 +440,6 @@ data.raw.wall["stone-wall"].resistances = {
 	{ type = "physical", decrease = 3, percent = 20 },
 	{ type = "impact", decrease = 45, percent = 60 },
 	{ type = "explosion", decrease = 50, percent = 80 },
-	{ type = "kr-explosion", percent = 100 },
 	{ type = "kr-radioactive", percent = 100 },
 	{ type = "fire", percent = 100 },
 	{ type = "acid", percent = 80 },
