5379746bc31c251560bffce92d6a642a524a141e
Polka37 <209836559+Polka37@users.noreply.github.com>
2026-02-08 23:55:57 +0100

    remove all uncessesary scripts

--- control.lua
+++ control.lua
@@ -1,10 +1,7 @@
 local handler = require("__core__.lualib.event_handler")
 
 handler.add_libraries({
-	require("__flib__.gui"),
-
 	require("scripts.freeplay"),
-	require("scripts.roboport"),
 })
 
 require("scripts.milestones")
