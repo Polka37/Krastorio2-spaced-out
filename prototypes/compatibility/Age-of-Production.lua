308b56a0d64847dbb207aee1245d50568d7b3e1e
Polka37 <209836559+Polka37@users.noreply.github.com>
2026-06-06 19:22:10 +0200

    AoP hotfix

--- prototypes/compatibility/Age-of-Production.lua
+++ prototypes/compatibility/Age-of-Production.lua
@@ -3,14 +3,13 @@ if not mods["Age-of-Production"] then
 end
 local data_util = require("data-util")
 
-data_util.add_prerequisite("aop-woodworking", "kr-greenhouse")
-data_util.add_prerequisite("aop-woodworking", "agricultural-science-pack")
+data_util.add_prerequisite("aop-lumber-mill", "kr-greenhouse")
+data_util.add_prerequisite("aop-lumber-mill", "agricultural-science-pack")
 
-data_util.remove_prerequisite("aop-woodworking", "tree-seeding")
-data_util.remove_prerequisite("aop-woodworking", "production-science-pack")
-data.raw.technology["aop-woodworking"].research_trigger = { type = "mine-entity", entity = "kr-greenhouse" }
+data_util.remove_prerequisite("aop-lumber-mill", "tree-seeding")
+data_util.remove_prerequisite("aop-lumber-mill", "production-science-pack")
+data.raw.technology["aop-lumber-mill"].research_trigger = { type = "mine-entity", entity = "kr-greenhouse" }
 
 if not mods["lignumis"] then
 	data.raw.technology["tree-seeding"] = nil
 end
-
