6f3226ec976035b67a9624ddb55c7d803f2eee3e
Polka37 <209836559+Polka37@users.noreply.github.com>
2026-06-26 10:35:49 +0200

    fix cooling recipes

--- prototypes/updates/generate-tech-card-cooling.lua
+++ prototypes/updates/generate-tech-card-cooling.lua
@@ -2,50 +2,52 @@ local data_util = require("data-util")
 local flib_locale = require("__flib__.locale")
 
 for _, original_recipe in pairs(data.raw.recipe) do
-	if original_recipe.category == "kr-tech-cards" then
-		local recipe = table.deepcopy(original_recipe)
-		recipe.name = original_recipe.name .. "-cooling"
-		recipe.energy_required = original_recipe.energy_required / 2
-		recipe.category = "kr-tech-cards-cooling"
-		recipe.subgroup = "kr-tech-cards-cooling"
-		recipe.localised_name = { "", flib_locale.of(original_recipe), " ", { "recipe-name.kr-cooling" } }
-		recipe.hide_from_signal_gui = false
-		local science_pack = data.raw.item[original_recipe.name]
-		recipe.order = science_pack.order
-		recipe.icons = {
-			{ icon = science_pack.icon },
-			{ icon = "__space-age__/graphics/icons/fluid/fluoroketone-cold.png", scale = 0.26, shift = { 8, 8 } },
-		}
-		data:extend({ recipe })
-		data_util.add_or_replace_ingredient(
-			recipe.name,
-			"fluoroketone-cold",
-			{ type = "fluid", name = "fluoroketone-cold", amount = 10, fluidbox_index = 1 }
-		)
-		data_util.add_or_replace_ingredient(
-			recipe.name,
-			"kr-blank-tech-card",
-			{ type = "item", name = "kr-blank-tech-card", amount = 6 }
-		)
-		data_util.add_or_replace_product(
-			recipe.name,
-			original_recipe.name,
-			{ type = "item", name = original_recipe.name, amount = 6 }
-		)
-		data_util.add_or_replace_product(
-			recipe.name,
-			"fluoroketone-hot",
-			{ type = "fluid", name = "fluoroketone-hot", amount = 10, ignored_by_productivity = 10 }
-		)
-		data_util.add_recipe_unlock("kr-quantum-computer", recipe.name)
-		for _, ingredients in pairs(recipe.ingredients) do
-			--Add additional fluid to 3rd pipe input
-			if
-				ingredients.type == "fluid"
-				and ingredients.name ~= "fluoroketone-cold"
-				and ingredients.name ~= "muluna-astronomical-data"
-			then
-				ingredients.fluidbox_index = 3
+	if original_recipe.categories then
+		if original_recipe.categories[1] == data.raw.recipe["kr-singularity-tech-card"].categories[1] then
+			local recipe = table.deepcopy(original_recipe)
+			recipe.name = original_recipe.name .. "-cooling"
+			recipe.energy_required = original_recipe.energy_required / 2
+			recipe.categories = { "kr-tech-cards-cooling" }
+			recipe.subgroup = "kr-tech-cards-cooling"
+			recipe.localised_name = { "", flib_locale.of(original_recipe), " ", { "recipe-name.kr-cooling" } }
+			recipe.hide_from_signal_gui = false
+			local science_pack = data.raw.item[original_recipe.name]
+			recipe.order = science_pack.order
+			recipe.icons = {
+				{ icon = science_pack.icon },
+				{ icon = "__space-age__/graphics/icons/fluid/fluoroketone-cold.png", scale = 0.26, shift = { 8, 8 } },
+			}
+			data:extend({ recipe })
+			data_util.add_or_replace_ingredient(
+				recipe.name,
+				"fluoroketone-cold",
+				{ type = "fluid", name = "fluoroketone-cold", amount = 10, fluidbox_index = 1 }
+			)
+			data_util.add_or_replace_ingredient(
+				recipe.name,
+				"kr-blank-tech-card",
+				{ type = "item", name = "kr-blank-tech-card", amount = 6 }
+			)
+			data_util.add_or_replace_product(
+				recipe.name,
+				original_recipe.name,
+				{ type = "item", name = original_recipe.name, amount = 6 }
+			)
+			data_util.add_or_replace_product(
+				recipe.name,
+				"fluoroketone-hot",
+				{ type = "fluid", name = "fluoroketone-hot", amount = 10, ignored_by_productivity = 10 }
+			)
+			data_util.add_recipe_unlock("kr-quantum-computer", recipe.name)
+			for _, ingredients in pairs(recipe.ingredients) do
+				--Add additional fluid to 3rd pipe input
+				if
+					ingredients.type == "fluid"
+					and ingredients.name ~= "fluoroketone-cold"
+					and ingredients.name ~= "muluna-astronomical-data"
+				then
+					ingredients.fluidbox_index = 3
+				end
 			end
 		end
 	end
